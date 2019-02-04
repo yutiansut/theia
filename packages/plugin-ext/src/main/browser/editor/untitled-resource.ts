03ab1ee88aecff0fe415d2c24815d8d44361dd61
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-02-04 13:13:59 +0000

    fix #4244: file management vscode commands
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/plugin-ext/src/main/browser/editor/untitled-resource.ts
+++ packages/plugin-ext/src/main/browser/editor/untitled-resource.ts
@@ -18,7 +18,6 @@ import { ResourceResolver, Resource } from '@theia/core';
 import URI from '@theia/core/lib/common/uri';
 import { injectable } from 'inversify';
 import { Schemes } from '../../../common/uri-components';
-import { UriComponents } from '../../../common/uri-components';
 
 const resources = new Map<string, UntitledResource>();
 let index = 0;
@@ -47,7 +46,7 @@ export class UntitledResource implements Resource {
     }
 }
 
-export function createUntitledResource(content?: string, language?: string): UriComponents {
+export function createUntitledResource(content?: string, language?: string): UntitledResource {
     let extension;
     if (language) {
         for (const lang of monaco.languages.getLanguages()) {
@@ -59,6 +58,5 @@ export function createUntitledResource(content?: string, language?: string): Uri
             }
         }
     }
-    const resource = new UntitledResource(new URI().withScheme(Schemes.Untitled).withPath(`/Untitled-${index++}${extension ? extension : ''}`), content);
-    return monaco.Uri.parse(resource.uri.toString());
+    return new UntitledResource(new URI().withScheme(Schemes.Untitled).withPath(`/Untitled-${index++}${extension ? extension : ''}`), content);
 }
